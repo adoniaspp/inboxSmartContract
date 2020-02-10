a358a3c7da653532204f00afe3a92d67aa611bad
adoniaspp <adoniaspp@gmail.com>
2020-02-09 23:04:28 -0300

    test with mocha

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,3 +3,44 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+
+beforeEach(() => {
+    //Get a list of all accounts
+    web3.eth.getAccounts().then(fetchedAccounts => {
+        console.log(fetchedAccounts);
+    });
+    //Use one of those accounts to deploy
+
+    //the contract
+});
+
+describe('Inbox', () => {
+    it('deploy', () => {
+
+    })
+})
+
+/*class Car{
+    park(){
+        return 'stopped';
+    }
+    drive(){
+        return 'vroom';
+    }
+}
+
+let car;
+
+beforeEach(()=>{
+    car = new Car();
+});
+
+describe('Car', ()=>{
+    it('can park', ()=>{
+        assert.equal(car.park(), 'stopped');
+    })
+
+    it('can drive',()=>{
+        assert.equal(car.drive(), 'vroom')
+    })
+})*/
